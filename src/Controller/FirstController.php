ba3e12f80aeb2b95d5adc0412e23a48e7bc6ef05
Chaima <chaima.bouhlel@insat.u-carthage.tn>
2022-04-24 14:24:20 +0100

    Manipulate TWIGS: utiliser les filtres predefinis

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -3,13 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Config\Framework\RequestConfig;
 
 class FirstController extends AbstractController
 {
+
     #[Route('/first', name: 'app_first')]
     public function index(): Response
     {
@@ -19,11 +18,18 @@ class FirstController extends AbstractController
     }
 
     #[Route('/sayHello/{name}', name: 'say.hello')]
-    public function sayHello(Request $request,$name):Response
+    public function sayHello($name):Response
     {
-        dd($request);
         return $this->render('first/hello.html.twig',[
             'nom'=>$name
         ]);
+    }
+
+    #[Route('multi/{entier1<\d+>}/{entier2<\d+>}',
+        name:'multiplication'
+    )]
+    public function multiplication($entier1, $entier2):Response{
+        $resultat = $entier1 * $entier2;
+        return new Response("<h1>$resultat</h1>");
     }
 }
